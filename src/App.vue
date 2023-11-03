768bfe70c5472f909ce076afa0e4536ceea972c7
Natalia Walkiewicz <na.walkiewicz@gmail.com>
2023-11-03 22:17:01 +0100

    add todo

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { reactive, ref } from 'vue'
+import { reactive, ref, computed } from 'vue'
 
 const textHeader = ref('hello')
 const titleClass = ref('title')
@@ -23,22 +23,70 @@ function increment() {
   count.value++
 }
 
+
+let id = 0
+const newTodo = ref('')
+const hideCompleted = ref(false)
+const todos = ref([
+  { id: id++, text: 'Learn HTML', done: true },
+  { id: id++, text: 'Learn JavaScript', done: true },
+  { id: id++, text: 'Learn Vue', done: false }
+])
+
+const filteredTodos = computed(() => {
+  return hideCompleted.value ? todos.value.filter((t) => !t.done) : todos.value
+})
+
+function addTodo() {
+  todos.value.push({ id: id++, text: newTodo.value, done: false })
+  newTodo.value = ''
+}
+
+function removeTodo(todo) {
+  todos.value = todos.value.filter((t) => t !== todo)
+}
+
 </script>
 
 <template>
   <h1 :class="titleClass">{{ textHeader }}</h1>
+
   <p>Count is: {{ counter.count }}</p>
   <button @click="increment">count is: {{ count }}</button>
+
   <br><br>
+
   <input :value="text" @input="onInput" placeholder="Type here">
   <p>{{ text }}</p>
+
   <button @click="toggle">toggle</button>
   <h2 v-if="awesome">Vue is awesome!</h2>
   <h2 v-else>Oh no 😢</h2>
+
+
+  <form @submit.prevent="addTodo">
+    <input v-model="newTodo">
+    <button>Add Todo</button>
+  </form>
+  <ul>
+    <li v-for="todo in filteredTodos" :key="todo.id">
+      <input type="checkbox" v-model="todo.done">
+      <span :class="{ done: todo.done }">{{ todo.text }}</span>
+      <button @click="removeTodo(todo)">X</button>
+    </li>
+  </ul>
+  <button @click="hideCompleted = !hideCompleted">
+    {{ hideCompleted ? 'Show all' : 'Hide completed' }}
+  </button>
+
+
 </template>
 
 <style>
 .title {
   color: blue;
+}
+.done {
+  text-decoration: line-through;
 }
 </style>
